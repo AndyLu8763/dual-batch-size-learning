f236d744554ac3ab19e4baebefc9e0b04adf47e6
Kuan-Wei Lu <andylu6046@gmail.com>
2022-11-28 12:10:43 +0800

    dynamic memory usage

--- experimental/record_train_time/tf_record_train_time_an_epoch.py
+++ experimental/record_train_time/tf_record_train_time_an_epoch.py
@@ -18,6 +18,7 @@ from tensorflow.keras import layers
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.set_visible_devices(physical_devices[0], 'GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 # In[ ]:
