a935d90788d968c4105cdd4301fe69af98b3952f
Kuan-Wei Lu <andylu6046@gmail.com>
2023-09-27 17:19:23 +0800

    20230927, add parser.args(), add read_logs.py

---
+++ experimental/read_logs.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+
+if len(sys.argv) < 2:
+    print("read_logs: error: no input files")
+    sys.exit()
+if len(sys.argv) > 2:
+    print("read_logs: error: too many input files")
+    sys.exit()
+if '.npy' not in sys.argv[1]:
+    print('read_logs: error: logs is not a ".npy" file')
+
+logs = np.load(sys.argv[1], allow_pickle=True).item()
+epochs = len(logs['t'])
+for i in range(0, epochs):
+    print(f'Epoch {i + 1}/{epochs}')
+    print(
+        f'batch_size: {logs["batch_size"][i]: d}',
+        f'- resolution: {logs["resolution"][i]: d}',
+        f'- dropout_rate: {logs["dropout_rate"][i]: .1f}',
+        '\n',
+        end=''
+    )
+    print(
+        f'{logs["t"][i]: .0f}s',
+        f'- loss: {logs["loss"][i]: .4f}',
+        f'- accuracy: {logs["accuracy"][i]: .4f}',
+        f'- val_loss: {logs["val_loss"][i]: .4f}',
+        f'- val_accuracy: {logs["val_accuracy"][i]: .4f}',
+        f'- lr: {logs["lr"][i]: g}',
+        '\n',
+        end=''
+    )
+    print('----')
